f588e6d5cb77bf7003cb45ac71ab09a8f2ebd5d1
Anees <aneespengad4447@gmail.com>
2026-09-02 18:01:28 +0530

    feat(main): setup global application configurations and swagger
    - enable URI versioning and set global api prefix
    - configure swagger api documentation at docs endpoint
    - add global validation pipe with strict property whitelisting
    - register global jwt authentication and roles guards
    - configure cors policy with credentials and headers support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,52 @@
-import { NestFactory } from '@nestjs/core';
+// import dns from 'dns';
+// dns.setServers(['8.8.8.8', '8.8.4.4']);
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { RolesGuard } from './common/guards/roles.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.setGlobalPrefix('api');
+
+  // --- Swagger Setup ---
+  const config = new DocumentBuilder()
+    .setTitle('TutorFlow API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  // writeFileSync('./swagger.json', JSON.stringify(document, null, 2));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  const reflector = app.get(Reflector);
+  app.useGlobalGuards(new JwtAuthGuard(reflector), new RolesGuard(reflector));
+
+  app.enableCors({
+    // origin: ['http://localhost:3000'],
+    origin: true,
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
